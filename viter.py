d40fbcd50c57c694ee87b2649595b8ec84abf7f7
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-02-27 21:19:37 +0200

    Add a rudimentary command line.

--- viter.py
+++ viter.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-from gi.repository import Gtk, Gdk, Vte, GLib, Pango
+from gi.repository import Gtk, Vte, GLib, Pango
 
 
 class Terminal(Vte.Terminal):
@@ -18,14 +18,23 @@ class Terminal(Vte.Terminal):
         )
         self.set_font(Pango.FontDescription("Monospace 12.5"))
 
+
 class Window(Gtk.Window):
     def __init__(self, terminal):
         Gtk.Window.__init__(self, title="viter")
         self.connect("delete_event", Gtk.main_quit)
-        self.add(terminal)
+
+        box = Gtk.VBox()
+        self.add(box)
+        box.pack_start(terminal, True, True, 0)
+
+        command_line = Gtk.Entry()
+        command_line.override_font(Pango.FontDescription("Monospace 12.5"))
+        box.pack_start(command_line, False, True, 0)
+
         self.show_all()
 
-
 if __name__ == "__main__":
-    Window(Terminal(["/bin/bash"]))
+    term = Terminal(["/bin/bash"])
+    wind = Window(term)
     Gtk.main()
