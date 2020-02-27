af559c1d849f68d2598b75ab43ddc574df32a33e
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-02-27 18:49:12 +0200

    Make something ugly that pops up a terminal.

---
+++ viter.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import gi
+
+from gi.repository import Gtk, Vte, GLib, Pango
+
+
+class Terminal(Vte.Terminal):
+    def __init__(self, argv):
+        Vte.Terminal.__init__(self)
+        self.spawn_async(
+            Vte.PtyFlags.DEFAULT,
+            None,
+            argv,
+            None,
+            GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+            None,
+            None,
+            -1,
+        )
+
+
+class Window(Gtk.Window):
+    def __init__(self, terminal):
+        Gtk.Window.__init__(self, title="viter")
+        self.connect("delete_event", Gtk.main_quit)
+        self.add(terminal)
+        self.show_all()
+
+
+if __name__ == "__main__":
+    Window(Terminal(["/bin/python"]))
+    Gtk.main()
