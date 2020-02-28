c3a75791e8dc40d00bfde3c70bfde85e0dde68d2
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-02-28 17:23:38 +0200

    Add an option to specify the shell to run.

--- viter.py
+++ viter.py
@@ -1,6 +1,13 @@
 #!/bin/python3
 
-from gi.repository import Gtk, Vte, GLib, Pango
+import sys
+import os
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("Vte", "2.91")
+
+from gi.repository import Gtk, Vte, GLib, Pango  # noqa E402
 
 
 class Terminal(Vte.Terminal):
@@ -28,13 +35,19 @@ class Window(Gtk.Window):
         self.add(box)
         box.pack_start(terminal, True, True, 0)
 
-        command_line = Gtk.Entry()
+        command_line = Gtk.Entry(placeholder_text="sample text")
+        # `override_font` is deprecated.
+        # Nothing like this is exposed instead though.
         command_line.override_font(Pango.FontDescription("Monospace 12.5"))
         box.pack_start(command_line, False, True, 0)
 
         self.show_all()
 
+
 if __name__ == "__main__":
-    term = Terminal(["/bin/bash"])
+    child_argv = sys.argv[1:]
+    if child_argv == []:
+        child_argv = [os.environ["SHELL"]]
+    term = Terminal(child_argv)
     wind = Window(term)
     Gtk.main()
