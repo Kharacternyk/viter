b11453064aeb097ee95fc5c993caab9b7a15ce90
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-03-03 18:30:59 +0200

    Use shorter names for the frequently used vars.

--- viter.py
+++ viter.py
@@ -30,21 +30,21 @@ class Terminal(Vte.Terminal):
 
 
 class Window(Gtk.Window):
-    def prepare_command_line(self, text):
-        self.command_line.grab_focus()
-        Gtk.Entry.do_insert_at_cursor(self.command_line, text)
+    def prepare_bar(self, text):
+        self.bar.grab_focus()
+        Gtk.Entry.do_insert_at_cursor(self.bar, text)
 
     def scroll_terminal(self, count):
-        vadjustment = self.terminal.get_vadjustment()
+        vadjustment = self.term.get_vadjustment()
         current = vadjustment.get_value()
         desired = current + count
         vadjustment.set_value(desired)
-        self.update_status_line()
+        self.update_bar()
 
     def set_default_key_map(self):
         self.key_map = {
-            Gdk.KEY_colon: (lambda: self.prepare_command_line(":")),
-            Gdk.KEY_slash: (lambda: self.prepare_command_line("/")),
+            Gdk.KEY_colon: (lambda: self.prepare_bar(":")),
+            Gdk.KEY_slash: (lambda: self.prepare_bar("/")),
             Gdk.KEY_j: (lambda: self.scroll_terminal(1)),
             Gdk.KEY_k: (lambda: self.scroll_terminal(-1)),
         }
@@ -58,34 +58,34 @@ class Window(Gtk.Window):
             event.keyval == Gdk.KEY_space
             and event.state & Gdk.ModifierType.CONTROL_MASK > 0
         ):
-            if self.command_line.is_visible():
-                self.command_line.hide()
+            if self.bar.is_visible():
+                self.bar.hide()
                 self.mode = Mode.NORMAL
             else:
-                self.command_line.show()
+                self.bar.show()
                 self.mode = Mode.DETACHED
             return True
 
         if self.mode == Mode.DETACHED:
-            if not self.command_line.has_focus():
+            if not self.bar.has_focus():
                 self.handle_detached_key_press(event)
                 return True
 
     def command_handler(self, widget):
-        command = self.command_line.get_text()
+        command = self.bar.get_text()
         if command[0] == ":":
             try:
                 eval(command[1:])
             except Exception as err:
                 self.last_error_msg = str(err)
             finally:
-                self.terminal.grab_focus()
+                self.term.grab_focus()
         elif command[0] == "/":
             # TODO search.
             pass
 
-    def get_status_line_string(self):
-        vadjustment = self.terminal.get_vadjustment()
+    def get_status_string(self):
+        vadjustment = self.term.get_vadjustment()
         terminal_top = int(vadjustment.get_value())
         terminal_bottom = terminal_top + int(vadjustment.get_page_size())
         emsg = self.last_error_msg
@@ -95,26 +95,26 @@ class Window(Gtk.Window):
             f"{emsg}[{terminal_top}-{terminal_bottom}] ({int(vadjustment.get_upper())})"
         )
 
-    def update_status_line(self):
-        self.command_line.set_placeholder_text(self.get_status_line_string())
+    def update_bar(self):
+        self.bar.set_placeholder_text(self.get_status_string())
 
-    def derive_command_line_appearance(self):
+    def derive_bar_appearance(self):
         # `override_font` is deprecated.
         # Nothing like it is exposed instead though.
-        self.command_line.override_font(self.terminal.get_font())
+        self.bar.override_font(self.term.get_font())
 
-    def command_line_focus_in_handler(self, widget, event):
-        self.command_line.set_alignment(0)
+    def bar_focus_in_handler(self, widget, event):
+        self.bar.set_alignment(0)
         self.last_error_msg = ""
 
-    def command_line_focus_out_handler(self, widget, event):
-        self.command_line.set_alignment(1)
-        self.command_line.set_text("")
-        self.update_status_line()
+    def bar_focus_out_handler(self, widget, event):
+        self.bar.set_alignment(1)
+        self.bar.set_text("")
+        self.update_bar()
 
-    def command_line_key_press_handler(self, widget, event):
+    def bar_key_press_handler(self, widget, event):
         if event.keyval == Gdk.KEY_Escape:
-            self.terminal.grab_focus()
+            self.term.grab_focus()
             return True
 
     def __init__(self, terminal_shell_argv):
@@ -122,31 +122,27 @@ class Window(Gtk.Window):
         self.connect("delete_event", Gtk.main_quit)
         self.connect("key_press_event", self.key_press_handler)
 
-        self.terminal = Terminal(terminal_shell_argv)
-        self.terminal.connect("cursor_moved", lambda a: self.update_status_line())
+        self.term = Terminal(terminal_shell_argv)
+        self.term.connect("cursor_moved", lambda a: self.update_bar())
 
         self.box = Gtk.VBox()
         self.add(self.box)
-        self.box.pack_start(self.terminal, True, True, 0)
+        self.box.pack_start(self.term, True, True, 0)
 
-        self.command_line = Gtk.Entry()
-        self.command_line.connect("activate", self.command_handler)
-        self.command_line.connect(
-            "key_press_event", self.command_line_key_press_handler
-        )
-        self.command_line.connect(
-            "focus_out_event", self.command_line_focus_out_handler
-        )
-        self.command_line.connect("focus_in_event", self.command_line_focus_in_handler)
-        self.command_line.set_alignment(1)
+        self.bar = Gtk.Entry()
+        self.bar.connect("activate", self.command_handler)
+        self.bar.connect("key_press_event", self.bar_key_press_handler)
+        self.bar.connect("focus_out_event", self.bar_focus_out_handler)
+        self.bar.connect("focus_in_event", self.bar_focus_in_handler)
+        self.bar.set_alignment(1)
 
-        self.box.pack_start(self.command_line, False, True, 0)
+        self.box.pack_start(self.bar, False, True, 0)
 
-        self.derive_command_line_appearance()
+        self.derive_bar_appearance()
 
         self.show_all()
 
-        self.command_line.hide()
+        self.bar.hide()
         self.mode = Mode.NORMAL
         self.last_error_msg = ""
         self.set_default_key_map()
@@ -169,6 +165,6 @@ if __name__ == "__main__":
     child_argv = sys.argv[1:]
     if child_argv == []:
         child_argv = [os.environ["SHELL"]]
-    window = Window(child_argv)
+    win = Window(child_argv)
     read_config()
     Gtk.main()
