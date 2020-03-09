737186c25bd2291a2854fa988c99fe4bc95306a0
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-03-09 14:21:41 +0200

    Do not use underscored names in the config.
    The clashing of names is anyway unlikely.

--- viterrc.py
+++ viterrc.py
@@ -2,48 +2,48 @@
 win.term.set_audible_bell(False)
 win.term.set_scrollback_lines(2000)
 
-from gi.repository import Pango as _pango
-win.set_font(_pango.FontDescription("Monospace 12.5"))
+from gi.repository import Pango
+win.set_font(Pango.FontDescription("Monospace 12.5"))
 
 # Add the current time to the bar.
-_default_status_string_func = Window.get_status_string
+default_status_string_func = Window.get_status_string
 
-from datetime import datetime as _dt
+from datetime import datetime
 
-def _custom_status_string(window):
-    now = _dt.now().time().strftime("%H:%M")
-    return f"{_default_status_string_func(window)} |{now}|"
+def get_custom_status_string(window):
+    now = datetime.now().time().strftime("%H:%M")
+    return f"{default_status_string_func(window)} |{now}|"
 
-Window.get_status_string = _custom_status_string
+Window.get_status_string = get_custom_status_string
 
 # Map space to exit DETACHED mode.
 win.detached_mode_key_map[Gdk.KEY_space] = lambda: win.enter_normal_mode()
 
 # Set a **LIGHT** color scheme.
-def _c(string):
+def c(string):
     color = Gdk.RGBA()
     color.parse("#" + string)
     return color
 
 win.term.set_colors(
-    _c("000000"),
-    _c("FFFFFF"),
+    c("000000"),
+    c("FFFFFF"),
     [
-        _c("000000"),
-        _c("8b0000"),
-        _c("006400"),
-        _c("808000"),
-        _c("00008b"),
-        _c("8b008b"),
-        _c("008b8b"),
-        _c("FFFFFF"),
-        _c("000000"),
-        _c("8b0000"),
-        _c("006400"),
-        _c("808000"),
-        _c("00008b"),
-        _c("8b008b"),
-        _c("008b8b"),
-        _c("FFFFFF"),
+        c("000000"),
+        c("8b0000"),
+        c("006400"),
+        c("808000"),
+        c("00008b"),
+        c("8b008b"),
+        c("008b8b"),
+        c("FFFFFF"),
+        c("000000"),
+        c("8b0000"),
+        c("006400"),
+        c("808000"),
+        c("00008b"),
+        c("8b008b"),
+        c("008b8b"),
+        c("FFFFFF"),
     ],
 )
